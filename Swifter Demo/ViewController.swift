43cd41418e3db6fc444c1e28c514c2413f0ff0a7
Mac <m@ABCDEFG.local>
2016-07-12 14:02:03 +0800

    Initial Commit

---
+++ Swifter Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swifter Demo
+//
+//  Created by Mac on 16/7/12.
+//  Copyright (c) 2016年 GroupFly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
